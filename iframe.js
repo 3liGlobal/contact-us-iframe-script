194a7136d495a8f2e77c717056bb0c1ec8d2cc60
HoorUlAin-3liGlobal <hoor.ulain@3li.global>
2024-09-25 10:54:46 +0500

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -5,23 +5,21 @@
         var divElement = document.getElementById("swell-customer-identification");
         var email = divElement ? divElement.getAttribute("data-email") : null;
         const iframe = document.getElementById('iframeContactUsOOKAUAE');
+        console.log("this is iframe");
         console.log(iframe);
         if (!iframe) {
             console.log("Iframe not found yet...");
         } else {
             console.log(divElement);
-            if (email) { // If email is not null or empty and iframe exists
-                console.log(email);
+            if (email) { 
+                // If email is not null or empty and iframe exists
+                
+                console.log("this is email"+  email);
                 iframe.src = `https://3liglobal.github.io/Contact_Us-Form_OOKA_UAE?email=${encodeURIComponent(email)}`;
                 clearInterval(checkEmailInterval); // Stop checking once the email is found and iframe is ready
-            }else{
-                 console.log("email not found");
+            }
                  iframe.src = `https://3liglobal.github.io/Contact_Us-Form_OOKA_UAE`;
                  clearInterval(checkEmailInterval);
-            }
-         iframe.src = `https://3liglobal.github.io/Contact_Us-Form_OOKA_UAE`;
-                 clearInterval(checkEmailInterval);
-         
         }
     }, 500); // Check every half second
 });
