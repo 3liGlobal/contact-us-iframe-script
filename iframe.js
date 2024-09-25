cc3ae29ab739fb83fc6ca70eb693e6004d98c91f
HoorUlAin-3liGlobal <hoor.ulain@3li.global>
2024-09-25 11:00:20 +0500

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -1,25 +1,26 @@
- document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
     console.log("Script Started...");
-    
+
     var checkEmailInterval = setInterval(function () {
         var divElement = document.getElementById("swell-customer-identification");
         var email = divElement ? divElement.getAttribute("data-email") : null;
         const iframe = document.getElementById('iframeContactUsOOKAUAE');
-        console.log("this is iframe");
-        console.log(iframe);
+
+        console.log("Checking iframe and email...");
+
         if (!iframe) {
             console.log("Iframe not found yet...");
         } else {
-            console.log(divElement);
             if (email) { 
-                // If email is not null or empty and iframe exists
-                
-                console.log("this is email"+  email);
+                // If email is present and iframe exists
+                console.log("Email found: " + email);
                 iframe.src = `https://3liglobal.github.io/Contact_Us-Form_OOKA_UAE?email=${encodeURIComponent(email)}`;
                 clearInterval(checkEmailInterval); // Stop checking once the email is found and iframe is ready
+            } else {
+                console.log("No email found, using default URL.");
+                iframe.src = `https://3liglobal.github.io/Contact_Us-Form_OOKA_UAE`;
+                clearInterval(checkEmailInterval); // Stop checking after setting default iframe src
             }
-                 iframe.src = `https://3liglobal.github.io/Contact_Us-Form_OOKA_UAE`;
-                 clearInterval(checkEmailInterval);
         }
     }, 500); // Check every half second
 });
