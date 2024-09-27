d8c70d046d38d81fdccc4c745ed01a6e30157ae2
HoorUlAin-3liGlobal <hoor.ulain@3li.global>
2024-09-27 17:30:18 +0500

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -20,7 +20,29 @@ function initializeEnglishIframe() {
         }
     }, 500);
 }
-
+function initializeArabicIframe() {
+    const iframe = document.getElementById('iframeContactUsOOKAUAEArabic');
+    if (!iframe) return;
+ 
+    console.log("Arabic Script Started...");
+    iframe.src = 'https://3liglobal.github.io/Arabic_Contact_Us-Form_OOKA_UAE';
+    let email;
+ 
+    const checkEmailInterval = setInterval(function () {
+        const divElement = document.getElementById("swell-customer-identification");
+        email = divElement.hasAttribute("data-email") ? divElement.getAttribute("data-email") : null;
+ 
+        if (email) {
+            console.log("Email found: " + email);
+            //iframe.contentWindow.postMessage(email, "*");
+            iframe.src = `https://3liglobal.github.io/Arabic_Contact_Us-Form_OOKA_UAE?email=${encodeURIComponent(email)}`;
+            clearInterval(checkEmailInterval);
+        } else {
+            console.log("No email found, using default URL.");
+        }
+    }, 500);
+}
+ 
 // Run on initial load
 document.addEventListener("DOMContentLoaded", initializeEnglishIframe);
 
@@ -30,8 +52,14 @@ const observer = new MutationObserver((mutations) => {
         if (mutation.type === "childList") {
             const newLanguageButton = document.querySelector('.language-switcher_button');
             if (newLanguageButton) {
+               var language =  document.querySelector('.language-switcher_button').children[0].innerHTML;
+                if(language=='English'){
+                     newLanguageButton.addEventListener('click', initializeEnglishIframe);
+                }
                 //window.location.reload();
-                newLanguageButton.addEventListener('click', initializeEnglishIframe);
+               else{ 
+                   newLanguageButton.addEventListener('click', initializeArabicIframe);
+               }
             }
         }
     });
