0a3055f27d6e6b323b2fd0f59d158186e7137f5c
HoorUlAin-3liGlobal <hoor.ulain@3li.global>
2024-09-30 10:48:45 +0500

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -1,8 +1,11 @@
+Console.log("This is English Script");
+
+
 function initializeEnglishIframe() {
     const iframe = document.getElementById('iframeContactUsOOKAUAE');
     if (!iframe) return;
 
-    console.log("Script Started...");
+    console.log("English frame Initiliazzed...");
     iframe.src = 'https://3liglobal.github.io/Contact_Us-Form_OOKA_UAE';
     let email;
 
@@ -23,15 +26,15 @@ function initializeEnglishIframe() {
 function initializeArabicIframe() {
     const iframe = document.getElementById('iframeContactUsOOKAUAEArabic');
     if (!iframe) return;
- 
-    console.log("Arabic Script Started...");
+
+    console.log("Arabic frame Initiliazzed...");
     iframe.src = 'https://3liglobal.github.io/Arabic_Contact_Us-Form_OOKA_UAE';
     let email;
- 
+
     const checkEmailInterval = setInterval(function () {
         const divElement = document.getElementById("swell-customer-identification");
         email = divElement.hasAttribute("data-email") ? divElement.getAttribute("data-email") : null;
- 
+
         if (email) {
             console.log("Email found: " + email);
             //iframe.contentWindow.postMessage(email, "*");
@@ -42,32 +45,34 @@ function initializeArabicIframe() {
         }
     }, 500);
 }
- 
+
 // Run on initial load
-var language =  document.querySelector('.language-switcher_button').children[0].innerHTML;
- if(language=='English'){ 
-document.addEventListener("DOMContentLoaded", initializeArabicIframe );
-                }        
-               else{
-                   document.addEventListener("DOMContentLoaded", initializeEnglishIframe);
-               }
+var language = document.querySelector('.language-switcher_button').children[0].innerHTML;
+console.log(language);
+if (language == 'English') {
+    document.addEventListener("DOMContentLoaded", initializeArabicIframe);
+}
+else {
+    document.addEventListener("DOMContentLoaded", initializeEnglishIframe);
+}
 
-document.addEventListener("DOMContentLoaded", function() {
-  var targetElement =  document.querySelector('.language-switcher_button');
+document.addEventListener("DOMContentLoaded", function () {
+    var targetElement = document.querySelector('.language-switcher_button');
 
-targetElement.addEventListener("click", function() {
-    var language =  document.querySelector('.language-switcher_button').children[0].innerHTML;
-    console.log("Clicked english")
-      if(language=='English'){ 
-document.addEventListener("DOMContentLoaded", initializeArabicIframe );
-                }        
-               else{
-                   document.addEventListener("DOMContentLoaded", initializeEnglishIframe);
-               }
-    });  
+    targetElement.addEventListener("click", function () {
+        var language = document.querySelector('.language-switcher_button').children[0].innerHTML;
+        console.log(language);
+        console.log("Clicked english")
+        if (language == 'English') {
+            initializeArabicIframe()
+        }
+        else {
+           initializeEnglishIframe();
+        }
+    });
 });
 
-              
+
 
 
 // MutationObserver to detect changes when the button is clicked
@@ -76,14 +81,14 @@ const observer = new MutationObserver((mutations) => {
         if (mutation.type === "childList") {
             const newLanguageButton = document.querySelector('.language-switcher_button');
             if (newLanguageButton) {
-               var language =  document.querySelector('.language-switcher_button').children[0].innerHTML;
-                if(language=='English'){
-                     newLanguageButton.addEventListener('click', initializeEnglishIframe);
+                var language = document.querySelector('.language-switcher_button').children[0].innerHTML;
+                if (language == 'English') {
+                    newLanguageButton.addEventListener('click', initializeEnglishIframe);
                 }
                 //window.location.reload();
-               else{ 
-                   newLanguageButton.addEventListener('click', initializeArabicIframe);
-               }
+                else {
+                    newLanguageButton.addEventListener('click', initializeArabicIframe);
+                }
             }
         }
     });
